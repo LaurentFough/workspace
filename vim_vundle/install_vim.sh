b8f1d8c9229eeb53207e5b26d50059c7c6264d5f
Hubert <mathsmac@yahoo.fr>
2014-04-12 23:36:35 +0200

    modified:   .gitignore modified:   vim_vundle/install_vim.sh

--- vim_vundle/install_vim.sh
+++ vim_vundle/install_vim.sh
@@ -15,39 +15,40 @@ do
 		continue
 	fi
 	link=${file/$suffixe/"$HOME/."}
-	$echo $file
-	$echo "   -> $link"
+	echo $file
+	echo "   -> $link"
   if [ -r $link ]
   then
     if [ "$1" == "--force" ]
     then
-      $echo -e "${orange}   -> écrase ancienne version${neutre}"
+      echo -e "${orange}   -> écrase ancienne version${neutre}"
       rm $link
       ln -s $file $link
     else
-      $echo -e "${rougefonce}   -> déjà présent !${neutre}"
+      echo -e "${rougefonce}   -> déjà présent !${neutre}"
     fi
   else
-    $echo -e "${vertfonce}   -> ok !${neutre}"
+    echo -e "${vertfonce}   -> ok !${neutre}"
     ln -s $file $link
   fi
 done
 
 link=~/.vim/bundle/vundle
-$echo vundle
-$echo "   -> $link"
+echo vundle
+echo "   -> $link"
 
 if [ -r $link ]
 then
   if [ "$1" == "--force" ]
   then
-    $echo -e "${orange}   -> écrase ancienne version${neutre}"
+    echo -e "${orange}   -> écrase ancienne version${neutre}"
     rm -rf $link
     git clone https://github.com/gmarik/Vundle.vim.git $link
   else
-    $echo -e "${rougefonce}   -> déjà présent !${neutre}"
+    echo -e "${rougefonce}   -> déjà présent !${neutre}"
   fi
 else
-  $echo -e "${vertfonce}   -> ok !${neutre}"
+  echo -e "${vertfonce}   -> ok !${neutre}"
   git clone https://github.com/gmarik/Vundle.vim.git $link
 fi
+vim +PluginInstall +qall
